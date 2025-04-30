8070414ec08c8640f176893c2c872c24882f09c2
Bhavani <96565325+Shilpabhavani1@users.noreply.github.com>
2025-04-30 14:03:06 +0530

    Time: 28 ms (96.39%) | Memory: 17.8 MB (22.97%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.py
@@ -0,0 +1,10 @@
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        r=""
+        for i in address:
+            if i==".":
+                r+="[.]"
+            else:
+                r+=i
+        return r
+        
